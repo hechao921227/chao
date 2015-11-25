fa1183a734f4ec8bfc123292ac4b68413fa73331
hechao <1617995604@qq.com>
2015-11-24 20:23:57 -0800

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by qianfeng on 15/11/24.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by qianfeng on 15/11/24.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
